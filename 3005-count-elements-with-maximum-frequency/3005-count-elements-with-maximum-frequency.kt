4ee52fde33e147fd9897e7f07210eb63245250f1
Dharshini.P.G <115150167+dharshinipg16@users.noreply.github.com>
2024-03-08 18:42:55 +0530

    Time: 172 ms (75.00%), Space: 35.3 MB (79.17%) - LeetHub

---
+++ 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.kt
@@ -0,0 +1,21 @@
+class Solution {
+    fun maxFrequencyElements(numbers: IntArray): Int {
+        val frequencyMap = mutableMapOf<Int, Int>()
+        for (number in numbers) {
+            frequencyMap[number] = frequencyMap.getOrDefault(number, 0) + 1
+        }
+
+        var totalCount = 0
+        var maxFrequency = Int.MIN_VALUE
+        for (frequency in frequencyMap.values) {
+            maxFrequency = maxOf(maxFrequency, frequency)
+        }
+
+        for (frequency in frequencyMap.values) {
+            if (frequency == maxFrequency) {
+                totalCount += maxFrequency
+            }
+        }
+        return totalCount
+    }
+}
