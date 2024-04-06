29256debbb79a92351427dca6ad60679f6a072a8
Dharshini.P.G <115150167+dharshinipg16@users.noreply.github.com>
2024-04-06 08:12:11 +0530

    Time: 44 ms (40.31%), Space: 44.7 MB (57.68%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    result[0] = i;
+                    result[1] = j;
+                    return result;
+                }
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = solution.twoSum(nums, target);
+        System.out.println(Arrays.toString(result)); // Output: [0, 1]
+    }
+}
