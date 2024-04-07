e50ed47d84ced4ba2863975a61919182c3bca8a6
Dharshini.P.G <115150167+dharshinipg16@users.noreply.github.com>
2024-04-07 13:10:09 +0530

    Time: 3 ms (45.50%), Space: 7.4 MB (39.07%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
-    
     bool checkValidString(string& s) {
+        
         int n=s.size();
         int dp[2][102]={0};// reduced space
         dp[n&1][0]=1;//base case
+        
         for(int i=n-1; i>=0; i--){
             char c=s[i];
             for(int balance=0; balance<=n; balance++){
