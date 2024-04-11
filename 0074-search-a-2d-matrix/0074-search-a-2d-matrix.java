171979f2fc656e29e0461df143876368bb965628
Dharshini.P.G <115150167+dharshinipg16@users.noreply.github.com>
2024-04-11 11:44:49 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (84.90%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -2,9 +2,17 @@ class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         for (int[] i : matrix) {
             if (i[0]<=target && i[i.length-1]>=target){
-                for (int ele: i) {
-                    if (ele == target){
+                int[] nums=i;
+                int start = 0;
+                int end = nums.length - 1;
+                while (start <= end) {
+                    int mid = (start + end) / 2;
+                    if (nums[mid] == target) {
                         return true;
+                    } else if (nums[mid] > target) {
+                        end = mid - 1;
+                    } else {
+                        start = mid + 1;
                     }
                 }
             }
@@ -12,6 +20,11 @@ class Solution {
         return false;
     }
 }
+
+
+
+
+
 
 // class Solution:
 //     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
