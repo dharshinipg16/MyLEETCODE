66382ca59fe2f66f28b0470080625beb73239190
Dharshini.P.G <115150167+dharshinipg16@users.noreply.github.com>
2024-04-11 09:08:57 +0530

    Time: 48 ms (16.26%), Space: 44.8 MB (41.10%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -15,11 +15,4 @@ public class Solution {
         return result;
     }
 
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        int[] nums = {2, 7, 11, 15};
-        int target = 9;
-        int[] result = solution.twoSum(nums, target);
-        System.out.println(Arrays.toString(result)); // Output: [0, 1]
-    }
 }
