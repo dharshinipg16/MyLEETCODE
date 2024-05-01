b898267028bda157f66f20ba9af85cef8f7e4984
Dharshini.P.G <115150167+dharshinipg16@users.noreply.github.com>
2024-05-01 12:58:37 +0530

    Time: 1 ms (100.00%), Space: 44.4 MB (66.37%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -1,26 +1,31 @@
 class Solution {
+    public int dfs (int i,int j,int n,int m,int[][] grid) {
+        if (i<0 || j<0 ||i>=n ||j>=m) return 0;
+        if (grid[i][j]==1) {
+            grid[i][j]=2; //visited
+            int sum=1;
+            int up = dfs(i-1,j,n,m,grid);
+            int down = dfs(i+1,j,n,m,grid);
+            int left = dfs(i,j-1,n,m,grid);
+            int right = dfs(i,j+1,n,m,grid);
+            sum += (up+down+right+left);
+            return sum;
+        }
+        return 0;
+    }
+    
     public int maxAreaOfIsland(int[][] grid) {
-        int max = 0;
-        for(int i = 0; i < grid.length; i++) {
-            for(int j = 0; j < grid[0].length; j++){
-                if(grid[i][j] == 1){
-                    max = Math.max(max, dfs(grid, i, j));
+        int n=grid.length;
+        int m =grid[0].length;
+        int area=0;
+        for (int i=0;i<n;i++) {
+            for (int j=0;j<m;j++){
+                if (grid[i][j]==1){
+                    int temparea = dfs(i,j,n,m,grid);
+                    area = Math.max(area,temparea);
                 }
             }
         }
-        return max;
-    }
-    public int dfs(int[][] grid, int i, int j){
-        if(i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == 0){
-            return 0;
-        }
-        grid[i][j] = 0;
-        int area = 1;
-        area += dfs(grid, i + 1, j);
-        area += dfs(grid, i - 1, j);
-        area += dfs(grid, i, j + 1);
-        area += dfs(grid, i, j - 1);
         return area;
     }
-
 }
