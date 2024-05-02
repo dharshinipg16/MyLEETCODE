cd4e2759fd13b42c1aad3e3d8642268772bcd9ab
Dharshini.P.G <115150167+dharshinipg16@users.noreply.github.com>
2024-05-02 08:03:39 +0530

    Time: 10 ms (5.58%), Space: 42.8 MB (16.21%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,48 +1,41 @@
-import java.util.*;
-
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
-        
-        Queue<int[]> queue = new LinkedList<>();
-        
-        // Add all rotten oranges to the queue and count fresh oranges
-        int cntFresh = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] == 2) {
-                    queue.offer(new int[]{i, j, 0}); // {x, y, time}
-                } else if (grid[i][j] == 1) {
-                    cntFresh++;
+        int n=grid.length;
+        int m=grid[0].length;
+        int cntfresh=0;
+        Queue <int[]> q=new LinkedList<>(); 
+        for (int i=0;i<n;i++) {
+            for (int j=0;j<m;j++) {
+                if (grid[i][j]==2){
+                    q.offer(new int[]{i,j,0});
+                }
+                if (grid[i][j]==1) {
+                    cntfresh++;
                 }
             }
         }
         
-        int time = 0;
-        int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-        
-        // Perform BFS
-        while (!queue.isEmpty()) {
-            int[] curr = queue.poll();
-            int x = curr[0];
-            int y = curr[1];
-            int currTime = curr[2];
-            time = Math.max(time, currTime); // Update time
-            
-            for (int[] dir : directions) {
-                int newX = x + dir[0];
-                int newY = y + dir[1];
-                
-                if (newX >= 0 && newX < n && newY >= 0 && newY < m && grid[newX][newY] == 1) {
-                    grid[newX][newY] = 2; // Mark as rotten
-                    queue.offer(new int[]{newX, newY, currTime + 1}); // Add to queue with incremented time
-                    cntFresh--; // Decrease count of fresh oranges
+        int time=0;
+        int[][] directions = {{0,1},{1,0},{-1,0},{0,-1}};
+        //all rotten orange positions are stored
+        while (!q.isEmpty()){
+            int[] arr = q.poll();
+            int i = arr[0];
+            int j=arr[1];
+            int currTime = arr[2];
+            time = Math.max(time, currTime);
+            for (int ii=0;ii<4;ii++){
+                int nr = i+directions[ii][0];
+                int nc = j+directions[ii][1];
+                if (nr>=0 && nc>=0 && nr<n && nc<m && grid[nr][nc]==1){
+                    grid[nr][nc]=2;
+                    q.offer(new int[]{nr,nc,currTime+1});
+                    cntfresh--;
+                    System.out.println(cntfresh);
+                    
                 }
             }
         }
-        
-        // If there are still fresh oranges left, return -1
-        return cntFresh == 0 ? time : -1;
+        return (cntfresh==0)?time : -1;
     }
 }
