73158abb595821ae35c62c39165efcbb47add8a4
Dharshini.P.G <115150167+dharshinipg16@users.noreply.github.com>
2024-05-03 09:22:08 +0530

    Time: 77 ms (6.22%), Space: 53.3 MB (36.73%) - LeetHub

--- 0886-possible-bipartition/0886-possible-bipartition.java
+++ 0886-possible-bipartition/0886-possible-bipartition.java
@@ -1,44 +1,52 @@
 class Solution {
     public boolean possibleBipartition(int n, int[][] dislikes) {
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        int[] colored = new int[n+1];
-        Arrays.fill(colored,-1);
-        
-        for (int i=0;i<n+1;i++){
+        for (int i=0;i<=n;i++) {
             adj.add(new ArrayList<>());
         }
-        for (int[] box:dislikes) {
+        for (int[] box: dislikes) {
             adj.get(box[0]).add(box[1]);
             adj.get(box[1]).add(box[0]);
         }
-        for (int i=0;i<=n;i++) {
-            if (colored[i]==-1) {
-                if (!bfs(adj,colored,i)){
-                    return false;
+        Queue<Integer> q = new LinkedList<>();
+        int[] colored = new int[n+1];
+        Arrays.fill(colored,-1); //initially all uncolored
+        q.offer(1);
+        colored[1]=1;
+        //boolean flag=true;
+        while(!q.isEmpty()) {
+            int cur = q.poll();
+            for(int child: adj.get(cur)){
+                if (colored[child]==-1){
+                    colored[child] = (colored[cur]==1)?0:1;
+                    q.offer(child);
                 }
             }
         }
-        return true;
-    }
-    public boolean bfs(ArrayList<ArrayList<Integer>> adj, int[] colored, int start) {
-        Queue<Integer> q = new LinkedList<>();
-        q.offer(start);
-        colored[start]=1;
-        while (!q.isEmpty()) {
-            int cur = q.poll();
-            int curcolor = colored[cur];
-            for (int child: adj.get(cur)) {
-                if (colored[child]==-1){
-                    colored[child]=1-curcolor;
-                    q.offer(child);
-                }
-                else {
-                    if (colored[child] == curcolor) {
-                        return false;
+        for (int i=0;i<n;i++) {
+            if (colored[i]==-1){
+                q.offer(i);
+                while(!q.isEmpty()) {
+                    int cur = q.poll();
+                    for(int child: adj.get(cur)){
+                        if (colored[child]==-1){
+                            colored[child] = (colored[cur]==1)?0:1;
+                            q.offer(child);
+                        }
                     }
                 }
             }
         }
+        for (int i:colored) {
+            System.out.print(i+" ");
+        }
+        System.out.println();
+        for (int[] box: dislikes) {
+            if (colored[box[0]]==colored[box[1]]){
+                return false;
+            }
+        }
         return true;
+        
     }
 }
