a8b3a51d13f38c7aad44f38e8f9244ea061fd9e7
Dharshini.P.G <115150167+dharshinipg16@users.noreply.github.com>
2024-05-03 09:21:30 +0530

    Time: 14 ms (82.76%), Space: 51.4 MB (77.96%) - LeetHub

--- 0886-possible-bipartition/0886-possible-bipartition.java
+++ 0886-possible-bipartition/0886-possible-bipartition.java
@@ -1,43 +1,41 @@
-import java.util.*;
-
 class Solution {
     public boolean possibleBipartition(int n, int[][] dislikes) {
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        for (int i = 0; i <= n; i++) {
+        int[] colored = new int[n+1];
+        Arrays.fill(colored,-1);
+        
+        for (int i=0;i<n+1;i++){
             adj.add(new ArrayList<>());
         }
-        for (int[] box : dislikes) {
+        for (int[] box:dislikes) {
             adj.get(box[0]).add(box[1]);
-            adj.get(box[1]).add(box[0]); // Don't forget to add both edges for an undirected graph
+            adj.get(box[1]).add(box[0]);
         }
-        
-        int[] colored = new int[n + 1];
-        Arrays.fill(colored, -1); // Initially all uncolored
-        
-        for (int i = 1; i <= n; i++) {
-            if (colored[i] == -1) {
-                if (!bfs(adj, colored, i))
+        for (int i=0;i<=n;i++) {
+            if (colored[i]==-1) {
+                if (!bfs(adj,colored,i)){
                     return false;
+                }
             }
         }
         return true;
     }
-    
-    private boolean bfs(ArrayList<ArrayList<Integer>> adj, int[] colored, int start) {
+    public boolean bfs(ArrayList<ArrayList<Integer>> adj, int[] colored, int start) {
         Queue<Integer> q = new LinkedList<>();
         q.offer(start);
-        colored[start] = 1;
-        
+        colored[start]=1;
         while (!q.isEmpty()) {
             int cur = q.poll();
-            int curColor = colored[cur];
-            
-            for (int child : adj.get(cur)) {
-                if (colored[child] == -1) {
-                    colored[child] = 1 - curColor; // Assign opposite color
+            int curcolor = colored[cur];
+            for (int child: adj.get(cur)) {
+                if (colored[child]==-1){
+                    colored[child]=1-curcolor;
                     q.offer(child);
-                } else if (colored[child] == curColor) {
-                    return false; // Found adjacent vertices with same color
+                }
+                else {
+                    if (colored[child] == curcolor) {
+                        return false;
+                    }
                 }
             }
         }
