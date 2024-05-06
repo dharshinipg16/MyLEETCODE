e23b66ecc452c8aa8965b0729f5b961578260d96
Dharshini.P.G <115150167+dharshinipg16@users.noreply.github.com>
2024-05-06 10:02:53 +0530

    Time: 15 ms (74.97%), Space: 52 MB (63.54%) - LeetHub

--- 0886-possible-bipartition/0886-possible-bipartition.java
+++ 0886-possible-bipartition/0886-possible-bipartition.java
@@ -4,6 +4,7 @@ class Solution {
         for (int i=0;i<=n;i++) {
             adj.add(new ArrayList<>());
         }
+        //create the adjacent list
         for (int[] box: dislikes) {
             adj.get(box[0]).add(box[1]);
             adj.get(box[1]).add(box[0]);
@@ -11,18 +12,25 @@ class Solution {
         Queue<Integer> q = new LinkedList<>();
         int[] colored = new int[n+1];
         Arrays.fill(colored,-1); //initially all uncolored
+        
+        //offer the first node and set the color to 1
         q.offer(1);
         colored[1]=1;
-        //boolean flag=true;
+        
         while(!q.isEmpty()) {
             int cur = q.poll();
+            //get the child of the current node and assign opposite colors to them, add them in the queue
             for(int child: adj.get(cur)){
                 if (colored[child]==-1){
                     colored[child] = (colored[cur]==1)?0:1;
                     q.offer(child);
+                }else {
+                    if (colored[child]==colored[cur]) return false;
                 }
+                
             }
         }
+        //in case there are disjoint components in the graph, travers through the uncolored nodes and do the same process of q as above
         for (int i=0;i<n;i++) {
             if (colored[i]==-1){
                 q.offer(i);
@@ -32,20 +40,18 @@ class Solution {
                         if (colored[child]==-1){
                             colored[child] = (colored[cur]==1)?0:1;
                             q.offer(child);
+                        } else {
+                            if (colored[child]==colored[cur]) return false;
                         }
                     }
                 }
             }
         }
-        for (int i:colored) {
-            System.out.print(i+" ");
-        }
-        System.out.println();
-        for (int[] box: dislikes) {
-            if (colored[box[0]]==colored[box[1]]){
-                return false;
-            }
-        }
+        // for (int i:colored) {
+        //     System.out.print(i+" ");
+        // }
+        //traverse
+        
         return true;
         
     }
