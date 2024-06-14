54ca43f973cb2ddcdbe430eaeffc47ad85590b45
Dharshini.P.G <115150167+dharshinipg16@users.noreply.github.com>
2024-06-14 12:59:18 +0530

    Time: 2 ms (46.11%), Space: 7.7 MB (86.9%) - LeetHub

---
+++ 3447-clear-digits/3447-clear-digits.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string clearDigits(string s) {
+        string res;
+        for (char ch: s){
+            if (isdigit(ch))
+                res.pop_back();
+            else
+                res+=ch;
+        }
+        return res;
+    }
+};
