27226c545ec1286c545986360e2d0a150860fc5d
Dharshini.P.G <115150167+dharshinipg16@users.noreply.github.com>
2024-07-05 08:52:02 +0530

    Time: 4 ms (100%), Space: 60.7 MB (44.14%) - LeetHub

--- 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
@@ -20,28 +20,27 @@ class Solution {
         
         //starts at 2nd node
         //breaks at last node itself, anyway last node is not a critical node
-        boolean flag = false;
+        boolean flag = false; // to make sure the distance updation starts aftr any one critical pointe encounterance
         while (cur.next!=null) {
             if ((cur.val < prev.val && cur.val < cur.next.val) || (cur.val > prev.val && cur.val > cur.next.val)) {
                 //it is a critical node, check the distances
                 flag = true;
                 if (dis1 < min && dis1!=0) {
                     min = dis1;
-                    System.out.println("MIN UPDATE"+cur.val+" "+dis1);
+                    // System.out.println("MIN UPDATE"+cur.val+" "+dis1);
                 }
                 if (dis2 > max && dis2!=0) {
                     max = dis2;
-                    System.out.println("MAX UPDATE"+cur.val+" "+dis2);
+                    // System.out.println("MAX UPDATE"+cur.val+" "+dis2);
                 }
-                dis1=1; //reset dis1 to 0, min dis is dis bet 2 critical nodes
+                dis1=1; //reset dis1 to 1, min dis is dis bet 2 critical nodes
                 dis2+=1;//dis bet first and last cri nodes, so no need to reset
             } else if (flag == true) {
-                System.out.println("NOT A CRITICAL POINT "+cur.val+" "+flag);
+                // System.out.println("NOT A CRITICAL POINT "+cur.val+" "+flag);
                 dis1+=1;
                 dis2+=1;
             }
             
-             
             prev=cur;
             cur=cur.next;
         }
